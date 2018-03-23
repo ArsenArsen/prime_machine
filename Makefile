93d4c2a5e3dec5b47662b084bad27f8d9d5c1681
ArsenArsen <arsenarsentmc@outlook.com>
2018-03-23 21:26:35 +0100

    change up the makefile, format the code

--- Makefile
+++ Makefile
@@ -2,8 +2,9 @@ CXX= g++
 LIBS= -pthread -std=c++11
 OUTPUT= primer
 DEBUG=
+WARN=-W -Wall -Wextra -Werror
 OPTIMIZATION=-O2
 
-all:
-	$(CXX) $(DEBUG) $(OPTIMIZATION) $(LIBS) main.cpp -o $(OUTPUT)
+primer: main.cpp
+	$(CXX) $(DEBUG) $(OPTIMIZATION) $(WARN) $(LIBS) main.cpp -o $(OUTPUT)
 
--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
 // vim: set foldmethod=marker :
 // vim: set foldlevel=0 :
 #include <algorithm>
+#include <atomic>
+#include <cmath>
+#include <cstdint>
 #include <iostream>
 #include <mutex>
+#include <new>
 #include <string>
 #include <thread>
 #include <vector>
-#include <cmath>
-#include <atomic>
-#include <new>
 
 namespace primer {
-    int concurrency = std::thread::hardware_concurrency();
-    std::vector<std::thread *> threads;
-    int max = 1;
-    std::mutex locker;
-    std::atomic_bool *sieve;
+    static unsigned concurrency = std::thread::hardware_concurrency();
+    static uint64_t max = 1;
+    static std::atomic_bool *sieve;
 
-    void runner(unsigned long long startpoint, unsigned long long maximum) {
-        for (int i = startpoint; i < maximum; i += concurrency) {
-            unsigned long long i2 = i * i;
-            for (int j = 0;; j++) {
-                unsigned long long current = i2 + j * i;
+    static void runner(uint64_t startpoint, uint64_t maximum) {
+        for (uint64_t i = startpoint; i < maximum; i += concurrency) {
+            uint64_t i2 = i * i;
+            for (uint64_t j = 0;; j++) {
+                uint64_t current = i2 + j * i;
                 if (current >= max) break;
                 sieve[current] = true;
             }
@@ -32,14 +31,18 @@ namespace primer {
 int main(int argc, char *argv[]) {
     std::ios::sync_with_stdio(false);
     std::vector<std::string> args(argv + 1, argv + argc);
-    if (args.size() == 0 || (primer::max = std::stoll(args[0])) < 2) return 1;
+    if (args.size() == 0 || (primer::max = std::stoull(args[0])) < 2) return 1;
+
     primer::sieve = new std::atomic_bool[primer::max];
     std::fill_n(primer::sieve, primer::max, false);
-    unsigned long long maximum = ceil(sqrt(primer::max));
-    for (int i = 0; i < primer::concurrency; i++) primer::threads.push_back(new std::thread(primer::runner, i + 2, maximum));
-    for (std::thread *t : primer::threads) t->join();
+    uint64_t maximum = uint64_t(floor(sqrt(primer::max)));
 
-    for (int i = 2; i < primer::max; i++) {
+    std::vector<std::thread *> threads;
+    for (unsigned i = 0; i < primer::concurrency; i++)
+        threads.push_back(new std::thread(primer::runner, i + 2, maximum));
+    for (std::thread *t : threads) t->join();
+
+    for (uint64_t i = 2; i < primer::max; i++) {
         if (!primer::sieve[i]) std::cout << i << '\n';
     }
 
