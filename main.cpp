ce6faf8f858da6a484d6204e79cb5a53b5eff675
ArsenArsen <arsenarsentmc@outlook.com>
2018-03-23 14:45:35 +0100

    add atomics

--- main.cpp
+++ main.cpp
@@ -7,13 +7,15 @@
 #include <thread>
 #include <vector>
 #include <cmath>
+#include <atomic>
+#include <new>
 
 namespace primer {
     int concurrency = std::thread::hardware_concurrency();
     std::vector<std::thread *> threads;
     int max = 1;
     std::mutex locker;
-    bool *sieve;
+    std::atomic_bool *sieve;
 
     void runner(unsigned long long startpoint, unsigned long long maximum) {
         for (int i = startpoint; i < maximum; i += concurrency) {
@@ -31,7 +33,7 @@ int main(int argc, char *argv[]) {
     std::ios::sync_with_stdio(false);
     std::vector<std::string> args(argv + 1, argv + argc);
     if (args.size() == 0 || (primer::max = std::stoll(args[0])) < 2) return 1;
-    primer::sieve = new bool[primer::max];
+    primer::sieve = new std::atomic_bool[primer::max];
     std::fill_n(primer::sieve, primer::max, false);
     unsigned long long maximum = ceil(sqrt(primer::max));
     for (int i = 0; i < primer::concurrency; i++) primer::threads.push_back(new std::thread(primer::runner, i + 2, maximum));
